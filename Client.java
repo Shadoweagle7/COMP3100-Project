952985d842f10070606a529491faca9b5222ea83
Shadoweagle7 <38882778+Shadoweagle7@users.noreply.github.com>
2021-04-02 15:49:03 +1100

    Reformatted the code so we are using Java SE 7's Automatic Resource Management. Preallocated a buffer for all messages instead of having to allocate memory for every single message. When transforming those bytes into a String, use the constructor String(byte[] bytes, int offset, int length). Added more error messages for the edge cases where failure may happen.

--- Client.java
+++ Client.java
@@ -4,90 +4,99 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Client {
-    static DataInputStream din;
-    static DataOutputStream dout;
-    static byte[] received;
+    public static final int MAX_BUFFER = 32;
 
     public static void printErrorMessage(String expected, String actual) {
         System.out.println("Expected: " + expected + " | Actual: " + actual);
     }
 
     public static void main(String[] args) throws Exception {
-        Socket s = new Socket("localhost", 50000);
+        // Using Java SE 7's Automatic Resource Management to call close() for us, since these objects implement the AutoCloseable interface.
+        // This works even if any of the code throws an exception.
+        try (
+            Socket s = new Socket("localhost", 50000);
+            DataInputStream din = new DataInputStream(s.getInputStream());
+            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+        ) {
+            byte[] received = new byte[MAX_BUFFER];
 
-        din = new DataInputStream(s.getInputStream());
-        dout = new DataOutputStream(s.getOutputStream());
-
-        send("HELO");
+            send(dout, "HELO");
         
-        receive(2, "OK");
+            receive(received, din, "OK");
 
-        send("AUTH bob");
-        
-        receive(2, "OK");
+            send(dout, "AUTH bob");
+            
+            receive(received, din, "OK");
 
-        send("REDY");
+            send(dout, "REDY");
 
-        receive(32); // JOBN ...
+            receive(received, 32, din); // JOBN ...
 
-        byte[] jobn = Arrays.copyOf(received, received.length); // Store for later
+            byte[] jobn = Arrays.copyOf(received, received.length); // Store for later
 
-        send("GETS All");
+            send(dout, "GETS All");
 
-        receive(12); // DATA nRecs recLen
+            receive(received, 12, din); // DATA nRecs recLen
 
-        DataCommand dataCommand = (DataCommand)parseCommand(new String(received));
+            DataCommand dataCommand = (DataCommand)parseCommand(new String(received));
 
-        int[] dataBytes = dataCommand.execute();
-        int totalDataSize = dataBytes[0] * dataBytes[1];
+            int[] dataBytes = dataCommand.execute();
+            int totalDataSize = dataBytes[0] * dataBytes[1];
 
-        ArrayList<String> servers = new ArrayList<String>();
+            ArrayList<String> servers = new ArrayList<String>();
 
-        while (!(new String(received).equals("."))) {
-            receive(totalDataSize);
+            while (!(new String(received).equals("."))) {
+                receive(received, totalDataSize, din);
 
-            String serverStateAll = new String(received);
-            String[] serverStates = serverStateAll.split("\n");
+                String serverStateAll = new String(received);
+                String[] serverStates = serverStateAll.split("\n");
 
-            for (String serverState : serverStates) {
-                servers.add(serverState);
+                for (String serverState : serverStates) {
+                    servers.add(serverState);
+                }
             }
+
+            // TODO: LSTJ
+
+            send(dout, "OK");
+
+            send(dout, "QUIT");
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
-        // TODO: LSTJ
-
-        send("OK");
-
-        send("QUIT");
-
-        dout.close();
-        din.close();
-        s.close();
     }
 
-    public static void send(String toSend) throws IOException {
+    public static void send(DataOutputStream dout, String toSend) throws IOException {
         dout.write(toSend.getBytes());
         dout.flush();
         System.out.println("Sending " + toSend);
     }
 
-    public static void receive(int size) throws IOException {
-        received = new byte[size];
-        din.read(received);
-        System.out.println("Server: " + new String(received));
+    public static void receive(byte[] data, int expectedSize, DataInputStream din) throws IOException {
+        if (din.read(data) != expectedSize) {
+            System.out.println("Server: " + new String(data, 0, expectedSize));
+            throw new IllegalArgumentException("Server gave unexpected response");
+        }
+
+        System.out.println("Server: " + new String(data, 0, expectedSize));
     }
 
-    public static void receive(int size, String expectedString) throws IOException {
-        received = new byte[size];
-        din.read(received);
+    public static void receive(byte[] data, DataInputStream din, String expectedString) throws IOException, IllegalArgumentException {
+        int expectedSize = expectedString.length();
 
-        String receivedString = new String(received);
+        if (din.read(data) != expectedSize) {
+            System.out.println("Server: " + new String(data, 0, expectedSize));
+            throw new IllegalArgumentException("Server gave unexpected response");
+        }
+
+        String receivedString = new String(data, 0, expectedSize);
         System.out.println("Server: " + receivedString);
         
         if (!receivedString.equals(expectedString)) {
             printErrorMessage(expectedString, receivedString);
 
-            // TODO: throw
+            throw new IllegalArgumentException("Server sent unknown message");
         }
     }
 
@@ -98,7 +107,11 @@ public class Client {
     public static class DataCommand extends Command {
         private int numberOfRecords, recordLength;
 
-        public DataCommand(int nRec, int recLen) {
+        public DataCommand(int nRec, int recLen) throws IllegalArgumentException {
+            if (nRec <= 0 || recLen <= 0) {
+                throw new IllegalArgumentException("Cannot have 0 or less records. Each record's size must be greater than 0");
+            }
+
             this.numberOfRecords = nRec;
             this.recordLength = recLen;
         }
@@ -108,7 +121,12 @@ public class Client {
         }
     }
 
-    public static Command parseCommand(String str) throws NumberFormatException, ArrayIndexOutOfBoundsException, IllegalArgumentException {
+    public static Command parseCommand(String str) 
+        throws NumberFormatException, ArrayIndexOutOfBoundsException, IllegalArgumentException, NullPointerException {
+        if (str == null || str.length() == 0) {
+            throw new NullPointerException("Invalid input");
+        }
+
         String[] args = str.split(" ");
 
         if (args == null || args.length == 0) {
